14e6e7c0b679f7ed8c8080e3aa845d7a66d7edac
Zylanx <32674125+Zylanx@users.noreply.github.com>
2022-12-06 21:14:27 +1100

    Add eq to misc-utils

--- misc-utils.nix
+++ misc-utils.nix
@@ -1,6 +1,10 @@
 let
     lib = import <nixpkgs/lib>;
 
+    # a -> a -> Boolean
     isMax = x: y: x > y;
+
+    # a -> a -> Boolean
+    eq = x: y: x == y;
 in
-    { inherit isMax; }
+    { inherit isMax eq; }
