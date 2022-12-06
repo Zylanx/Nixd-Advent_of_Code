46ee47d516193796823800eefebe9f88c3f8b184
Zylanx <32674125+Zylanx@users.noreply.github.com>
2022-12-06 19:44:50 +1100

    Refactored with suggestions from ners

--- Day1/2.nix
+++ Day1/2.nix
@@ -1,27 +1,26 @@
 #!/usr/bin/env -S nix-instantiate --eval --strict --arg nothing null
 { input_file ? null }:
 let
-    inherit (builtins) map foldl';
+    inherit (builtins) map;
     inherit (import <nixpkgs/lib>) take;
 
     utils = import ../utils.nix;
-    inherit (utils) lists strings misc args;
-    inherit (utils.pipe) pipef;
+    inherit (utils) lists strings args;
+    inherit (utils.pipe) pipe pipef;
 
-    input_file_contents = pipef [ args.files.fromPath strings.strip ] input_file;
+    input_file_contents = pipe input_file [ args.files.fromPath strings.strip ];
 
     # String -> [Int]
     toInventory = pipef [ strings.splitLines lists.toInts ];
 
-    # [Int] -> Ints
-    totalInventory = lists.total;
-
-    elf_invs = map (pipef [ toInventory totalInventory ]) (strings.groupLines input_file_contents);
+    elf_invs = pipe input_file_contents [
+        strings.groupLines
+        (map toInventory)
+        (map lists.total)
+    ];
 
     sorted_invs = lists.sortMax elf_invs;
 
     top_three = take 3 sorted_invs;
-
-    totalled = lists.total top_three;
 in
-    totalled
+    lists.total top_three
